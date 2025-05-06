8476e88745c5eb54d884d666f25df8f88c363e9e
Martin Treml <git@treml.dev>
2025-05-07 00:05:17 +0200

    Add hyprpanel

--- home/bundles/hypr-desktop.nix
+++ home/bundles/hypr-desktop.nix
@@ -4,6 +4,7 @@
     hyprland.enable = true;
     hyprlock.enable = true;
     hyprpaper.enable = true;
+    hyprpanel.enable = true;
 
     # Enable Hyprland services
     services.hypridle.enable = true;
---
+++ home/features/hyprpanel.nix
@@ -0,0 +1,10 @@
+{ ... }: {
+  # Enable HyprPanel in home-manager
+  programs.hyprpanel = {
+    enable = true;
+
+    hyprland.enable = true;
+
+    overwrite.enable = true;
+  };
+}
