53897beb869b2233fe4e85c29030ef070a8d8292
Martin Treml <martin.treml@agilox.net>
2025-10-28 16:31:19 +0100

    Remove unused features and services

--- users/martin/home-whirl.nix
+++ users/martin/home-whirl.nix
@@ -7,8 +7,6 @@
     discord.enable = true;
     moonlight.enable = true;
     nvf.enable = true;
-    rustdesk.enable = true;
-    services.flameshot.enable = true;
     signal-desktop.enable = true;
     ghostty.enable = true;
   };
