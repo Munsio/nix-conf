0d97b778b1242212698dadee70b54ca78581718b
Martin Treml <martin.treml@agilox.net>
2025-11-07 09:26:46 +0100

    Zen-Browser via home-manager on manjaro does not work

--- users/martin/home-whirl.nix
+++ users/martin/home-whirl.nix
@@ -8,6 +8,7 @@
     moonlight.enable = true;
     signal-desktop.enable = true;
     ghostty.enable = true;
+    zen-browser.enable = true;
   };
 
   ## Git
@@ -15,6 +16,31 @@
     git = lib.mkIf config.homeModules.git.enable {
       userEmail = "git@treml.dev";
       userName = "Martin Treml";
+    };
+  };
+
+  ## Custom Mime Apps
+  xdg = {
+    mimeApps = {
+      enable = true;
+      associations.added = {
+        "application/json" = ["zen-twilight.desktop"];
+        "application/pdf" = ["zen-twilight.desktop"];
+        "application/x-extension-htm" = ["zen-twilight.desktop"];
+        "application/x-extension-html" = ["zen-twilight.desktop"];
+        "application/x-extension-shtml" = ["zen-twilight.desktop"];
+        "application/x-extension-xht" = ["zen-twilight.desktop"];
+        "application/x-extension-xhtml" = ["zen-twilight.desktop"];
+        "application/xhtml+xml" = ["zen-twilight.desktop"];
+        "text/html" = ["zen-twilight.desktop"];
+        "text/plain" = ["zen-twilight.desktop"];
+        "x-scheme-handler/about" = ["zen-twilight.desktop"];
+        "x-scheme-handler/chrome" = ["zen-twilight.desktop"];
+        "x-scheme-handler/http" = ["zen-twilight.desktop"];
+        "x-scheme-handler/https" = ["zen-twilight.desktop"];
+        "x-scheme-handler/mailto" = ["zen-twilight.desktop"];
+        "x-scheme-handler/unknown" = ["zen-twilight.desktop"];
+      };
     };
   };
 }
--- users/martin/home.nix
+++ users/martin/home.nix
@@ -52,7 +52,6 @@ in {
     starship.enable = true;
     vscode.enable = true;
     yazi.enable = true;
-    zen-browser.enable = true;
     zoxide.enable = true;
 
     services = {
@@ -61,31 +60,6 @@ in {
   };
 
   # Custom configuration
-
-  ## Custom Mime Apps
-  xdg = {
-    mimeApps = {
-      enable = true;
-      associations.added = {
-        "application/json" = ["zen-twilight.desktop"];
-        "application/pdf" = ["zen-twilight.desktop"];
-        "application/x-extension-htm" = ["zen-twilight.desktop"];
-        "application/x-extension-html" = ["zen-twilight.desktop"];
-        "application/x-extension-shtml" = ["zen-twilight.desktop"];
-        "application/x-extension-xht" = ["zen-twilight.desktop"];
-        "application/x-extension-xhtml" = ["zen-twilight.desktop"];
-        "application/xhtml+xml" = ["zen-twilight.desktop"];
-        "text/html" = ["zen-twilight.desktop"];
-        "text/plain" = ["zen-twilight.desktop"];
-        "x-scheme-handler/about" = ["zen-twilight.desktop"];
-        "x-scheme-handler/chrome" = ["zen-twilight.desktop"];
-        "x-scheme-handler/http" = ["zen-twilight.desktop"];
-        "x-scheme-handler/https" = ["zen-twilight.desktop"];
-        "x-scheme-handler/mailto" = ["zen-twilight.desktop"];
-        "x-scheme-handler/unknown" = ["zen-twilight.desktop"];
-      };
-    };
-  };
 
   ## Hyprland
   wayland.windowManager.hyprland.settings = {
