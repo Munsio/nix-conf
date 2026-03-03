707793cccc0be3fd21e3f861fc7a63545a82f073
Martin Treml <git@treml.dev>
2026-03-03 18:09:45 +0100

    Add tailscale

---
+++ modules/bundles/tailscale.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  # make the tailscale command usable to users
+  environment.systemPackages = [pkgs.tailscale];
+
+  # enable the tailscale service
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "client";
+  };
+}
