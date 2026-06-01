d888f6e1f5f8070f4c3f24fc959691de0058a37a
Martin Treml <git@treml.dev>
2026-06-01 11:08:19 +0200

    Pin kernel because of bluetooth bug
    Kernels after 7.0.6 have some problems with MediaTek Wifi/Bluetooth
    cards

--- modules/features/systemd-boot.nix
+++ modules/features/systemd-boot.nix
@@ -10,8 +10,17 @@
       };
     };
     tmp.cleanOnBoot = true;
-    kernelPackages =
-      pkgs.linuxPackages_latest; # _zen, _hardened, _rt, _rt_latest, etc.
+    # kernelPackages = pkgs.linuxPackages_latest; # _zen, _hardened, _rt, _rt_latest, etc.
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_7_0.override {
+      argsOverride = rec {
+        src = pkgs.fetchurl {
+          url = "mirror://kernel/linux/kernel/v7.x/linux-${version}.tar.xz";
+          sha256 = "sha256-y6REQKpXr/18ISQdxbwjSw31PEmfj/w+vCkN0zkKdSM=";
+        };
+        version = "7.0.6";
+        modDirVersion = "7.0.6";
+      };
+    });
 
     # Silent boot
     kernelParams = [
