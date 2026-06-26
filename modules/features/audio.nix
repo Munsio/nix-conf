70999786af9682fd2caf62dbd190b2f1f6605dcb
Martin Treml <git@treml.dev>
2026-06-26 23:06:00 +0200

    remove 32bit libraries

--- modules/features/audio.nix
+++ modules/features/audio.nix
@@ -6,7 +6,6 @@
       pipewire = {
         enable = true;
         alsa.enable = true;
-        alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
         wireplumber = {
